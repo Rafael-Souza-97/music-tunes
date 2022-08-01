4e7b311130c0b5fc2d0a8271c28f3bb95af139ea
Rafael Souza <souza_rafael.97@outlook.com>
2022-08-01 15:31:09 -0300

    Adiciona Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import PageNotFound from './pages/PageNotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+
+        <Switch>
+          <Route exact path="/" render={ () => <Login /> } />
+          <Route exact path="/search" render={ () => <Search /> } />
+          <Route exact path="/album/:id" render={ () => <Album /> } />
+          <Route exact path="/favorites" render={ () => <Favorites /> } />
+          <Route exact path="/profile" render={ () => <Profile /> } />
+          <Route exact path="/profile/edit" render={ () => <ProfileEdit /> } />
+          <Route render={ () => <PageNotFound /> } />
+        </Switch>
+      </div>
+    );
   }
 }
 
