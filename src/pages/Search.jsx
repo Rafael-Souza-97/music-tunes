bcc9576a7fe4e166e835ac1c6d174d5016a291d0
Rafael Souza <souza_rafael.97@outlook.com>
2022-08-01 23:12:27 -0300

    Adiciona Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isSearchButtonDisabled: true,
+  };
+
+  onInputSearchChange = ({ target }) => {
+    const maxNumber = 2;
+    if (target.value.length >= maxNumber) {
+      this.setState({ isSearchButtonDisabled: false });
+    } else {
+      this.setState({ isSearchButtonDisabled: true });
+    }
+  }
+
   render() {
+    const { isSearchButtonDisabled } = this.state;
     return (
       <div>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <input
+            type="text"
+            name="inputArtistBand"
+            data-testid="search-artist-input"
+            onChange={ this.onInputSearchChange }
+          />
+
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isSearchButtonDisabled }
+            // onClick={ this.handleSubmit }
+          >
+            Enviar
+          </button>
+        </div>
       </div>
     );
   }
